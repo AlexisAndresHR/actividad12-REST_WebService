166b1883dbb3d809c007020ac910a8452e75bb9f
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2019-03-25 21:24:14 -0600

    Funciones de obtención y traducción de datos JSON en la app completas

--- webservices/getAllClients.php
+++ webservices/getAllClients.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = "d5x4ae6ze2og6sjo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$db_name = "euwd9h6p5k605t8z";
-$db_user = "cc6hx3z3huxqsogi";
-$db_password = "u99oplxd3ojzupx0";
+$db_host = "localhost";
+$db_name = "act11_webservice_aahr";
+$db_user = "aahr";
+$db_password = "aahr.2019";
 
 $connection = mysql_connect($db_host, $db_user, $db_password) or die("Connection Error: " . mysql_error());
 
